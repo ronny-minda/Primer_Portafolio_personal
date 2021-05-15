3fa88059f5916d7f341b89c2619721e6ba5e96bc
Ronny Minda <ronny.michael.minda.vera@gmail.com>
2021-05-14 22:42:48 -0500

    animacion terminada

--- javascript/index.js
+++ javascript/index.js
@@ -1,5 +1,41 @@
 const menu = document.getElementById('btn-menu');
+const hover1 = document.getElementById('hover1');
+const hover2 = document.getElementById('hover2');
+const hover3 = document.getElementById('hover3');
 
+var hover;
+
+
+
+/* MENU DESPLEGABLE */
 menu.addEventListener('click', () => {
     document.getElementById('menu').classList.toggle('menu-activo');
 });
+
+
+/* 
+hover1.addEventListener("mouseover",(e)=>{
+    document.getElementById('menu').classList.toggle('menu-activo');
+});
+hover1.addEventListener("mouseout",(e)=>{
+    alert("hover1 salida");
+});
+
+
+hover2.addEventListener("mouseover",(e)=>{
+    alert("hove2 entrada");
+});
+hover2.addEventListener("mouseout",(e)=>{
+    alert("hover2 salida");
+});
+ */
+
+
+
+/* hover3.addEventListener("mouseover",(e)=>{
+    doument.getElementById('cambio3').classList.toggle('prende');
+
+});
+hover3.addEventListener("mouseout",(e)=>{
+    document.getElementById('cambio3').classList.toggle('prende');
+}); */
